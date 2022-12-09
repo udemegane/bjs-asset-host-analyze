0b4500692417020db0a7158e3782ca002e53a8b0
udemegane <cappuccino1204.ck@gmail.com>
2022-12-09 21:02:55 +0900

    簡単な動作確認

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { NullEngine, Scene, SceneLoader } from "@babylonjs/core";
+import { NullEngine, Scene, SceneLoader, Skeleton } from "@babylonjs/core";
 import { SkeletonAnalyzer } from "./skeletonAnalyzer";
 import "@babylonjs/loaders/glTF";
 // @ts-ignore
@@ -6,16 +6,26 @@ import xhr2 from "xhr2";
 
 export const main = () => {
   global.XMLHttpRequest = xhr2;
-  const engine = new NullEngine();
-  const scene = new Scene(engine);
-  SceneLoader.ImportMesh(
-    "",
+  asyncFn();
+  // const engine = new NullEngine();
+  // const scene = new Scene(engine);
+};
+
+const asyncFn = async () => {
+  const meta = await SkeletonAnalyzer.AnalyzeFromFileAsync(
     "https://raw.githubusercontent.com/BabylonJS/Babylon.js/master/packages/tools/playground/public/scenes/",
-    "dummy2.babylon",
-    scene,
-    () => {
-      console.log("success!");
-    }
+    "dummy2.babylon"
   );
-  // SkeletonAnalyzer.AnalyzeFromFileAsync("sample/", "dummy2.babylon");
+  SkeletonAnalyzer.ShowMetadata(meta);
+  SkeletonAnalyzer.SaveMetadata(meta);
+  console.log();
+  const meta3 = await SkeletonAnalyzer.AnalyzeFromFileAsync(
+    "https://raw.githubusercontent.com/BabylonJS/Babylon.js/master/packages/tools/playground/public/scenes/",
+    "dummy3.babylon",
+    true,
+    true,
+    meta
+  );
+  SkeletonAnalyzer.ShowMetadata(meta3);
+  SkeletonAnalyzer.SaveMetadata(meta3);
 };
