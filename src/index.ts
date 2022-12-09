e7503ba8e702a2f660458b5a06636f44d7e77050
udemegane <cappuccino1204.ck@gmail.com>
2022-12-09 18:23:35 +0900

    First commit

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { Main } from "./main";
+
+const game = new Main();
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+//import * as Recast from 'recast-detour'
+import { NullEngine } from "@babylonjs/core";
+
+export class Main {
+  engine: NullEngine;
+
+  constructor() {
+    this.engine = new NullEngine();
+  }
+}
